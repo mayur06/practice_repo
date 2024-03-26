3972669bab43e274650318793462a604dc0c0ea9
Mayurjadhav11 <86162669+Mayurjadhav11@users.noreply.github.com>
2024-03-26 07:48:53 +0530

    the button page file data is change

--- src/test/java/Framework/test/butt_test.java
+++ src/test/java/Framework/test/butt_test.java
@@ -10,39 +10,47 @@ import org.testng.annotations.Test;
 import Framework.base.TestsBases;
 import Framework.page.butt_page;
 
-
 public class butt_test extends TestsBases {
 	butt_page btp;
-	
+
+	public butt_test() {
+		super();
+	}
+
 	@BeforeClass
 	public void butt_test_second() throws IOException {
 		setup();
-		btp=new butt_page();
+		btp = new butt_page();
 	}
-	
-	@Test (priority = 1)
+
+	@Test(priority = 1)
 	public void butt_test_third() throws InterruptedException {
+		test = extent.createTest("Test cases 13", "The users click on the click me buttons");
 		btp.butt_page_first();
 	}
-	
-	@Test (priority = 2)
+
+	@Test(priority = 2)
 	public void butt_test_fourth() {
-		List<String> click_response=btp.click_me_response();
-		
-		String actual_click_resp=click_response.get(0);
-		
-		String expected_click_resp="You have done a dynamic click";
-		
+		test = extent.createTest("Test Cases 14", "Validate the response of click me button");
+		List<String> click_response = btp.click_me_response();
+
+		String actual_click_resp = click_response.get(0);
+
+		String expected_click_resp = "You have done a dynamic click";
+
 		Assert.assertEquals(actual_click_resp, expected_click_resp);
 	}
-	
-	@Test (priority = 3)
+
+	@Test(priority = 3)
 	public void butt_test_fifth() throws InterruptedException {
+		test = extent.createTest("Test Cases 15", "The user click on right click button");
 		btp.butt_page_second();
 	}
-	
+
 	@AfterClass
 	public void teardown() {
+		extent.flush();
 		driver.close();
 	}
+
 }
