cd99dc844655ab551fadcb5132653499f7e85ee0
Mayurjadhav11 <86162669+Mayurjadhav11@users.noreply.github.com>
2024-03-27 07:32:34 +0530

    Project complete

--- src/test/java/Framework/test/practice_form_test.java
+++ src/test/java/Framework/test/practice_form_test.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -15,23 +16,26 @@ import Framework.page.practice_form_page;
 public class practice_form_test extends TestsBases {
 	practice_form_page pfp;
 	
-	@BeforeTest
-	public static void practice_form_test_first() throws IOException {
-		practice_form_test.setup();
+	public practice_form_test() {
+		super();
 	}
 	
-	@BeforeMethod
-	public void practice_form_test_second() {
+	
+	@BeforeClass
+	public void practice_form_test_second() throws IOException {
+		setup();
 		pfp=new practice_form_page();
 	}
 	
 	@Test (priority = 1)
 	public void practice_form_test_third() throws InterruptedException {
+		test=extent.createTest("Test case 18","Enter all the details in practice forms");
 		pfp.practice_form_page_first();
 	}
 	
 	@AfterClass
 	public void teardown() {
+		extent.flush();
 		driver.close();
 	}
 	
